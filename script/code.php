63e0f9e5164b3f31423b62970aed6613f39c0038
cybelle <60591224+verycooluser@users.noreply.github.com>
2025-05-24 18:07:23 -0400

    updated color defaults and nixed that uggo counter

--- script/code.php
+++
@@ -1,3 +0,0 @@
-ref=document.referrer;sw="none"; sd="none";sw=screen.width;sh=screen.height;sc=screen.colorDepth;
-qs = "&ref="+escape(ref);
-document.write('<scr' + 'ipt language="javascript" src="/script/show.php"></scr' + 'ipt>');
--- script/show.php
+++
@@ -1,7 +0,0 @@
-
-//document.write('<a target="_new" href="https://www.bravenet.com/"><img src="//assets.bravenet.com/common/images/counter/overlimit.gif" border="0" alt="Powered by Bravenet" /></a><br/>');
-document.write('<table cellpadding="0" cellspacing="0" border="0" align="center"><tr><td align="center"><img src="//counter2.bravenet.com/counter.php?count=2&type=125&cpv=3&key=1b3ca3c7fa85f3b3ec8893c57a9a6aaf" border="0"  alt="Bravenet Counter Stats" title="Bravenet Counter Stats"  />');
-document.write('<br />');
-            document.write('<a target="_new" href="http://www.bravenet.com/"><img src="//assets.bravenet.com/common/images/counter/tags/1_preview.gif" border="0" alt="Powered by Bravenet" /></a><br/>');
-                            document.write('<a style="font-size:12px;font-weight:bold;margin-top:3px;" target="_new" href="https://counter2.bravenet.com/v2/?id=408145&type=overview&usernum=92859121">View Statistics</a>');
-            document.write('</td></tr></table>');
