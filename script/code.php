0a7774ba2aa16df9d97e30c2575961a161f21af4
cybelle <60591224+verycooluser@users.noreply.github.com>
2025-05-23 20:45:18 -0400

    bravenet counter workaround

---
+++ script/code.php
@@ -0,0 +1,3 @@
+ref=document.referrer;sw="none"; sd="none";sw=screen.width;sh=screen.height;sc=screen.colorDepth;
+qs = "&ref="+escape(ref);
+document.write('<scr' + 'ipt language="javascript" src="https://pub2.bravenet.com/counter/show.php?id=408145&usernum=92859121&cpv=3'+qs+'"></scr' + 'ipt>');
